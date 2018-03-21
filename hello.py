57c7c99173416f512ffeabad389e45474781551f
miapatrucchi <32535589+miapatrucchi@users.noreply.github.com>
2018-03-21 20:40:15 +0000

    My First App

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # here we are defining our app 
 app = Flask ("MyApp")
@@ -16,5 +16,16 @@ def idontexist ():
 @app.route ("/MiaPatrucchi")
 def MiaPatrucchi ():
 	return "That's my name"
+
+@app.route ("/<name>")
+# the 'name' in brackets is a variable. THe {{}} in the html file are allowing us to make it variable in html
+def hello_someone(name):
+	return render_template("hello.html", name=name.title())
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+	form_data = request.form
+	print form_data["email"]
+	return "All OK"
 
 app.run(debug=True)
